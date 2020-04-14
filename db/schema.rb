e6efae40947a2425b6fa4b21cc07c6bda526a000
Daisaku Sawada <d_sawada@hotmail.co.jp>
2020-04-14 14:14:08 +0000

    2020/4/14 save1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200325123148) do
+ActiveRecord::Schema.define(version: 20200402134146) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "eventname"
+    t.string "place"
+    t.datetime "eventday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
